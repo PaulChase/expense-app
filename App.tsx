947c7fc4ac5deda79c3a86fbb5ed5a00d6e6a105
Ajonye Paul <ajonyechase@gmail.com>
2023-08-05 18:51:48 +0100

    add bottom navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import Home from "./app/HomeScreen";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer, useNavigationContainerRef } from "@react-navigation/native";
+import { Feather, Ionicons, FontAwesome5, Entypo, MaterialIcons, MaterialCommunityIcons } from "@expo/vector-icons";
+
+import HomeScreen from "./app/HomeScreen";
+import CategoriesScreen from "./app/CategoriesScreen";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+	return (
+		<NavigationContainer>
+			<Tab.Navigator
+				screenOptions={({ route }) => ({
+					tabBarIcon: ({ focused, color, size }) => {
+						switch (route.name) {
+							case "Home":
+								return <Ionicons name="home" size={24} color={color} />;
+							case "Bills":
+								return <FontAwesome5 name="money-check-alt" size={24} color={color} />;
+						}
+					},
+					tabBarActiveTintColor: "blue",
+					// tabBarInactiveTintColor: "blue",
+					tabBarLabelStyle: {
+						fontSize: 14,
+						paddingBottom: 5,
+					},
+					tabBarStyle: {
+						paddingVertical: 10,
+						height: 62,
+						// backgroundColor: colorScheme === "dark" ? "black" : "#bfbfbf",
+						borderTopWidth: 2,
+						borderTopColor: "gray",
+					},
+					headerShown: true,
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+					// headerTintColor: headerTextColor,
+					// headerRight: () => <DarkModeSwitch />,
+				})}
+			>
+				<Tab.Screen name="Home" component={HomeScreen} />
+				<Tab.Screen name="Bills" component={CategoriesScreen} />
+			</Tab.Navigator>
+		</NavigationContainer>
+	);
+}
---
+++ app/HomeScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+	return (
+		<View className=" bg-pink-500 p-6">
+			<Text>Home</Text>
+		</View>
+	);
+}
