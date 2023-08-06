d49f6dd402ff966d2415f6f165ed06b78cd2defb
Ajonye Paul <ajonyechase@gmail.com>
2023-08-06 01:24:23 +0100

    start fetching UBA debit and credit alerts

--- app/Components/TransactionItem.tsx
+++ app/Components/TransactionItem.tsx
@@ -2,11 +2,31 @@ import React from "react";
 import { Text, View } from "react-native";
 import { formatNumberInThousand } from "../utils/helpers";
 
-export default function TransactionItem() {
+interface MessageItem {
+	type: string;
+	amount: number;
+	date: string;
+}
+
+interface TransactionItemProps {
+	message: MessageItem;
+}
+
+export default function TransactionItem({ message }: TransactionItemProps) {
 	return (
-		<View className=" p-3 bg-white border-l-4 border-red-700 rounded-md mt-4">
-			<Text className=" text-xl text-red-600 font-bold mt-1">₦ {formatNumberInThousand(1200)}</Text>
-			<Text>Lorem ipsum dolor sit amet.</Text>
+		<View
+			className={`p-3 bg-white border-l-4  rounded-md mt-4 ${
+				message.type === "Credit" ? "border-green-600" : "border-red-500"
+			} `}
+		>
+			<Text
+				className={`text-xl text-red-600 font-bold mt-1 mb-2 ${
+					message.type === "Credit" ? "text-green-600" : "text-red-500"
+				}`}
+			>
+				₦ {formatNumberInThousand(message.amount)}
+			</Text>
+			<Text>{new Date(message.date).toDateString()}</Text>
 		</View>
 	);
 }
--- app/Screens/HomeScreen.tsx
+++ app/Screens/HomeScreen.tsx
@@ -18,23 +18,73 @@ const filter = {
 	 *    - Same for minDate but with "date >= minDate"
 	 */
 	minDate: 1554636310165, // timestamp (in milliseconds since UNIX epoch)
-	maxDate: 1556277910456, // timestamp (in milliseconds since UNIX epoch)
+	// maxDate: 1556277910456,
 	// bodyRegex: "(.*)How are you(.*)",
 
 	/** the next 5 filters should NOT be used together, they are OR-ed so pick one **/
-	read: 0, // 0 for unread SMS, 1 for SMS already read
-	_id: 1234, // specify the msg id
-	thread_id: 12, // specify the conversation thread_id
-	address: "+1888------", // sender's phone number
-	body: "How are you", // content to match
+	// read: 1, // 0 for unread SMS, 1 for SMS already read
+	// _id: 1234, // specify the msg id
+	// thread_id: 12, // specify the conversation thread_id
+	// address: "+1888------",
+	// body: "How are you",
 	/** the next 2 filters can be used for pagination **/
 	indexFrom: 0, // start from index 0
-	maxCount: 10, // count of SMS to return each time
+	maxCount: 50, // count of SMS to return each time
+	address: "UBA",
+};
+
+const getAmount = (msg: string) => {
+	// Regular expression to match the amount
+	const amountRegex = /Amt:NGN\s*([\d,\.]+)/;
+
+	// Extract the amount using the regular expression
+	const match = msg.match(amountRegex);
+
+	if (match && match[1]) {
+		const amount = match[1].replace(/,/g, ""); // Remove commas from the amount
+		return amount;
+	} else {
+		return null;
+	}
+};
+
+const formateDate = (theDate: number) => {
+	// Create a new Date object using the timestamp
+	const date = new Date(theDate);
+
+	// Extract the various components of the date
+	const year = date.getFullYear();
+	const month = date.getMonth() + 1; // Month is 0-indexed, so add 1
+	const day = date.getDate();
+	const hours = date.getHours();
+	const minutes = date.getMinutes();
+	const seconds = date.getSeconds();
+
+	// Create a formatted date string
+	const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+	return formattedDate;
+};
+
+const getBalance = (message: string) => {
+	// Regular expression to match the balance
+	const balanceRegex = /Bal:NGN\s*([\d,\.]+)/;
+
+	// Extract the balance using the regular expression
+	const match = message.match(balanceRegex);
+
+	if (match && match[1]) {
+		const balance = match[1].replace(/,/g, ""); // Remove commas from the balance
+		return balance; // Output: Balance: 27248.39
+	} else {
+		return null;
+	}
 };
 
 export default function HomeScreen() {
 	const [activeTab, setActiveTab] = useState<string>("all");
 	const [messages, setMessages] = useState([]);
+	const [balance, setBalance] = useState<string | null>("0");
 
 	const handleChangeTab = (tab: string) => {
 		setActiveTab(tab);
@@ -48,14 +98,30 @@ export default function HomeScreen() {
 			},
 			(count, smsList) => {
 				console.log("Count: ", count);
-				console.log("List: ", smsList);
-				var arr = JSON.parse(smsList);
+				// console.log("List: ", smsList);
+				const messages = JSON.parse(smsList);
 
-				arr.forEach(function (object) {
-					console.log("Object: " + object);
-					console.log("-->" + object.date);
-					console.log("-->" + object.body);
-				});
+				const recentBalance = getBalance(messages[0].body);
+
+				const mainData = messages
+					.filter((msg: any) => msg.body.startsWith("Txn:"))
+					.map((msg: any) => {
+						const type = msg.body.startsWith("Txn: Credit") ? "Credit" : "Debit";
+						const amount = getAmount(msg.body);
+						const date = formateDate(msg.date);
+
+						return { type, amount, date };
+					});
+				setBalance(recentBalance);
+				setMessages(mainData);
+
+				// arr.forEach(function (object) {
+				// 	// console.log("Object: " + object);
+				// 	console.log("-->" + object._id);
+				// 	console.log("-->" + object.date);
+				// 	console.log("-->" + object.address);
+				// 	console.log("-->" + object.body);
+				// });
 			}
 		);
 	}, []);
@@ -65,7 +131,7 @@ export default function HomeScreen() {
 			<ScrollView className=" p-4 pb-20">
 				<View className=" p-3 bg-white border-l-4 border-blue-700 rounded-md">
 					<Text>Balance</Text>
-					<Text className=" text-3xl text-blue-700 font-bold mt-2">₦ {formatNumberInThousand(45899889)}</Text>
+					<Text className=" text-3xl text-blue-700 font-bold mt-2">₦ {formatNumberInThousand(balance)}</Text>
 				</View>
 
 				<Text className=" font-semibold mt-6 text-lg">Transactions</Text>
@@ -77,8 +143,8 @@ export default function HomeScreen() {
 
 				{activeTab === "all" && (
 					<View className=" pb-20">
-						{new Array(10).fill(0).map((item, index) => (
-							<TransactionItem key={index} />
+						{messages.map((msg, index) => (
+							<TransactionItem message={msg} key={index} />
 						))}
 					</View>
 				)}
